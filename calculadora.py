3c12a84d42cc08c736d43a3b4159045865a15128
yreimond <cauaalmeida.7438@gmail.com>
2025-04-15 22:31:05 -0300

    Adciona potencia com loop for

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,9 @@ def subtracao(a, b):
 
 def multiplicacao(a, b):
     return a * b
+
+def potencia(base, expoente):
+    resultado = 1
+    for _ in range(expoente):
+        resultado *= base
+    return resultado
