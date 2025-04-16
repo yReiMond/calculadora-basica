72fa2ba191112e58217de44c68a773d54ae7af5b
yreimond <cauaalmeida.7438@gmail.com>
2025-04-15 22:35:42 -0300

    Resolve conflito: mantém operador ** para potencia (agora certo)

--- calculadora.py
+++ calculadora.py
@@ -7,7 +7,6 @@ def subtracao(a, b):
 def multiplicacao(a, b):
     return a * b
 
-<<<<<<< HEAD
 def divisao(a, b):
     if b == 0:
         raise ValueError("Divião por zero!")
@@ -15,10 +14,3 @@ def divisao(a, b):
 
 def potencia(base, expoente):
     return base ** expoente
-=======
-def potencia(base, expoente):
-    resultado = 1
-    for _ in range(expoente):
-        resultado *= base
-    return resultado
->>>>>>> feature-potencia-loop
